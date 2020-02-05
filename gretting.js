36811ac2d37391178bca8bed088d82ba690fa114
YUNINROK <inroktic@naver.com>
2020-02-05 14:49:18 +0900

    환영인사

--- gretting.js
+++ gretting.js
@@ -1,7 +1,10 @@
-const form = document.querySelector("js-form"),
-    input = form.querySelector("input");
-const USER_LS = "currentUser",
-    SHOWING_CN = "showing";
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser";
+const SHOWING_CN = "showing";
+
 function saveName(text) {
     localStorage.setItem(USER_LS,text);
 }
@@ -13,18 +16,18 @@ function handleSubmit(event) {
 }
 function askForName() {
     form.classList.add(SHOWING_CN);
-    form.addEventListener("submit", handleSubmit)
+    form.addEventListener("submit", handleSubmit);
 }
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
-    greeting.innerText = `Hello ${text}`
+    greeting.innerText = `Hello  ${text}`;
 }
 function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
-    if(currentUser === null){
+    if(currentUser === null){ // 유저가 없을 때
         askForName();
-    }else {
+    }else { // 유저가 있을 때
         paintGreeting(currentUser);
     }
 }
